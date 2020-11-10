a80a769edd0fba69d80acace06bdc2630bb3c095
Willian dos Santos Piva <willian.piva@edu.univali.br>
2020-11-09 21:02:11 -0300

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Windows;
+using System.Collections.Generic;
+using System.Windows.Controls;
+using System.Linq;
+
+namespace MatchGame
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            SetUpGame();
+        }
+
+        private void SetUpGame()
+        {
+            List<string> animalEmoji = new List<string>()
+            {
+                "🐒", "🐒",
+                "🐷", "🐷", 
+                "🐲", "🐲",
+                "🐍", "🐍",
+                "🐋", "🐋",
+                "🦑", "🦑",
+                "🐔", "🐔",
+                "🦅", "🦅",
+            };
+            Random random = new Random();
+            foreach(TextBlock textBlock in mainGrid.Children.OfType<TextBlock>())
+            {
+                int index = random.Next(animalEmoji.Count);
+                string nextEmoji = animalEmoji[index];
+                textBlock.Text = nextEmoji;
+                animalEmoji.RemoveAt(index);
+            }
+        }
+    }
+}
